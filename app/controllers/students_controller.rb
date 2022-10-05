f0ad41c214f3a632cfb59b5c719f80276f8525e1
Vasi1ek745 <vasi1ek745@gmail.com>
2022-10-05 14:28:05 +0500

    new edit delete for students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,25 +1,51 @@
 class StudentsController < ApplicationController
   def index
-  end
-
-  def index
+    @students = Student.all
   end
 
   def show
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new student_params     
+      if @student.save
+        flash[:success] = "Новый ученик добавлен"
+        redirect_to students_path
+      else
+        render :new
+      end
   end
 
   def edit
+    @student = Student.find params[:id]
   end
 
   def update
+     @student = Student.find params[:id]
+    if @student.update student_params
+      flash[:success] = "Данные изменены"
+      redirect_to students_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @student = Student.find params[:id]
+    if @student.destroy
+      flash[:success] = "Ученик удален"
+      redirect_to students_path , status: 303
+    else
+      render :edit
+    end
   end
+
+  private
+    def student_params
+      params.require(:student).permit(:name, :subject, :location, :pay_by_hour, :other, :class_in_school, :phone_number) 
+    end
 end
